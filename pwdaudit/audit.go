5c5057b6b829cd4f932ebe998a15a5d1ec7c420a
arshi016 <arshilife16@gmail.com>
2023-09-21 14:04:26 -0700

    Update audit.go

--- pwdaudit/audit.go
+++ pwdaudit/audit.go
@@ -4,115 +4,161 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
+	"unicode"
 )
 
 const (
-	LowerLetters = "abcdefghijklmnopqrstuvwxyz"
-	UpperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
-	Digits       = "0123456789"
-	Symbols      = "~!@#$%^&*()_+`-={}|[]\\:\"<>?,./"
+	commonPasswordsFile = "10k-most-common.txt"
+	dictionaryFile      = "words_alpha.txt"
 )
 
-var commonPasswords = make(map[string]bool)
-
-func loadCommonPasswords(filename string) error {
-	file, err := os.Open(filename)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		commonPasswords[scanner.Text()] = true
-	}
-
-	return scanner.Err()
-}
-
 type PasswordPolicy struct {
-	MinLength          int
-	MinUppercase       int
-	MinLowercase       int
-	MinDigit           int
-	MinSpecialChar     int
-	ExcludeSimilar     bool
-	GeneratePassphrase bool
+	MinLength        int
+	MinUppercase     int
+	MinLowercase     int
+	MinDigit         int
+	MinSpecialChar   int
+	ExcludeSimilar   bool
+	CheckCommonWords bool
+	CheckDictionary  bool
 }
 
-type Auditor struct {
-	policy PasswordPolicy
+type PasswordAuditor struct {
+	commonPasswords map[string]struct{}
+	dictionary      []string
+	policy          PasswordPolicy
 }
 
-func NewAuditor(policy PasswordPolicy) *Auditor {
-	return &Auditor{policy: policy}
+func NewPasswordAuditor(policy PasswordPolicy) *PasswordAuditor {
+	auditor := &PasswordAuditor{
+		commonPasswords: make(map[string]struct{}),
+		policy:          policy,
+	}
+
+	if policy.CheckCommonWords {
+		file, err := os.Open(commonPasswordsFile)
+		if err != nil {
+			panic(err)
+		}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			auditor.commonPasswords[scanner.Text()] = struct{}{}
+		}
+		file.Close()
+	}
+
+	if policy.CheckDictionary {
+		file, err := os.Open(dictionaryFile)
+		if err != nil {
+			panic(err)
+		}
+		scanner := bufio.NewScanner(file)
+		for scanner.Scan() {
+			auditor.dictionary = append(auditor.dictionary, scanner.Text())
+		}
+		file.Close()
+	}
+
+	return auditor
 }
 
-func (a *Auditor) AuditPassword(password string) {
-	if len(password) < a.policy.MinLength {
-		fmt.Printf("Password is too short. It should be at least %d characters long.\n", a.policy.MinLength)
-	}
+func (a *PasswordAuditor) AuditPassword(password string) {
+	// Pattern Matching
+	a.checkCommonPasswords(password)
+	a.checkDictionaryWords(password)
+	a.checkKeyboardPatterns(password)
+	a.checkL33tSpeak(password)
+	// ... Additional pattern checks can be added here
 
-	if countChars(password, UpperLetters) < a.policy.MinUppercase {
-		fmt.Printf("Password should contain at least %d uppercase letters.\n", a.policy.MinUppercase)
-	}
+	// Feedback System
+	a.provideFeedback(password)
 
-	if countChars(password, LowerLetters) < a.policy.MinLowercase {
-		fmt.Printf("Password should contain at least %d lowercase letters.\n", a.policy.MinLowercase)
-	}
-
-	if countChars(password, Digits) < a.policy.MinDigit {
-		fmt.Printf("Password should contain at least %d digits.\n", a.policy.MinDigit)
-	}
-
-	if countChars(password, Symbols) < a.policy.MinSpecialChar {
-		fmt.Printf("Password should contain at least %d symbols.\n", a.policy.MinSpecialChar)
-	}
-
-	if containsCommonPassword(password) {
-		fmt.Println("Password matches a commonly used password. Consider using a more unique password.")
-	}
-
-	// TODO: Add check for dictionary words and other policies here if required.
+	// Additional functionalities like Crack Time Estimation, Score System, etc.
+	// can be added in similar fashion
 }
 
-func countChars(s, chars string) int {
-	count := 0
-	for _, char := range s {
-		if strings.ContainsRune(chars, char) {
-			count++
+func (a *PasswordAuditor) checkCommonPasswords(password string) {
+	if _, exists := a.commonPasswords[password]; exists {
+		fmt.Println("Warning: The password is a common password.")
+	}
+}
+
+func (a *PasswordAuditor) checkDictionaryWords(password string) {
+	for _, word := range a.dictionary {
+		if strings.Contains(password, word) {
+			fmt.Println("Warning: The password contains a dictionary word.")
+			break
 		}
 	}
-	return count
 }
 
-func containsCommonPassword(password string) bool {
-	return commonPasswords[password]
+func (a *PasswordAuditor) checkKeyboardPatterns(password string) {
+	// Simple example: Check for "qwerty" pattern
+	if strings.Contains(password, "qwerty") {
+		fmt.Println("Warning: The password contains a keyboard pattern.")
+	}
+}
+
+func (a *PasswordAuditor) checkL33tSpeak(password string) {
+	leetPattern := regexp.MustCompile(`[a@4][b8][c<\(][d][e3][f][g9][h#][i1\|][j][k][l1\|][m][n][o0][p][q][r][s5\$][t7\+][u][v][w][x][y][z2]`)
+	if leetPattern.MatchString(password) {
+		fmt.Println("Warning: The password contains L33t speak.")
+	}
+}
+
+func (a *PasswordAuditor) provideFeedback(password string) {
+	if len(password) < a.policy.MinLength {
+		fmt.Println("Suggestion: Increase the length of your password.")
+	}
+
+	var upper, lower, digit, special int
+	for _, r := range password {
+		switch {
+		case unicode.IsUpper(r):
+			upper++
+		case unicode.IsLower(r):
+			lower++
+		case unicode.IsDigit(r):
+			digit++
+		case unicode.IsPunct(r) || unicode.IsSymbol(r):
+			special++
+		}
+	}
+
+	if upper < a.policy.MinUppercase {
+		fmt.Println("Suggestion: Use more uppercase characters.")
+	}
+	if lower < a.policy.MinLowercase {
+		fmt.Println("Suggestion: Use more lowercase characters.")
+	}
+	if digit < a.policy.MinDigit {
+		fmt.Println("Suggestion: Use more digits.")
+	}
+	if special < a.policy.MinSpecialChar {
+		fmt.Println("Suggestion: Use more special characters.")
+	}
 }
 
 func main() {
-	if err := loadCommonPasswords("10k-most-common.txt"); err != nil {
-		fmt.Printf("Failed to load common passwords: %v\n", err)
-		return
-	}
-
 	policy := PasswordPolicy{
-		MinLength:          40,
-		MinUppercase:       10,
-		MinLowercase:       10,
-		MinDigit:           10,
-		MinSpecialChar:     10,
-		ExcludeSimilar:     true,
-		GeneratePassphrase: false,
+		MinLength:        40,
+		MinUppercase:     10,
+		MinLowercase:     10,
+		MinDigit:         10,
+		MinSpecialChar:   10,
+		ExcludeSimilar:   true,
+		CheckCommonWords: true,
+		CheckDictionary:  true,
 	}
 
-	auditor := NewAuditor(policy)
+	auditor := NewPasswordAuditor(policy)
 
-	fmt.Println("Enter the password to audit:")
-	scanner := bufio.NewScanner(os.Stdin)
-	if scanner.Scan() {
-		password := scanner.Text()
-		auditor.AuditPassword(password)
-	}
+	// Take the password to be audited as input from the user
+	fmt.Print("Enter the password to be audited: ")
+	var password string
+	fmt.Scanln(&password)
+
+	auditor.AuditPassword(password)
 }
